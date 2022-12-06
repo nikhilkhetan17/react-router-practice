db75a7c08442d3b0e7710cc31ed6bed17758f61b
nikhilkhetan17 <iamnikhilkhetan@gmail.com>
2022-12-06 17:15:16 +0530

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Thismy Home page</h1>;
+};
+
+export default Home;
---
+++ src/components/Services.js
@@ -0,0 +1,5 @@
+const Services = () => {
+  return <h1>Thismy Services page</h1>;
+};
+
+export default Services;
